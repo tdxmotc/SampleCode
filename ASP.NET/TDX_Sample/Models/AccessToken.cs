57c6f96c5069ba7b0bc80b93371fa61a01beb24b
2111004_Wei <cano.cheng@iisigroup.com>
2022-02-17 17:16:05 +0800

    inital

---
+++ ASP.NET/TDX_Sample/Models/AccessToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TDX_Sample.Models
+{
+    public class AccessToken
+    {
+        public string access_token { get; set; }
+        public int expires_in { get; set; }
+        public int refresh_expires_in { get; set; }
+        public string token_type { get; set; }
+        public int notbeforepolicy { get; set; }
+        public string scope { get; set; }
+    }
+}
