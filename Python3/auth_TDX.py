8a45cd4e396e9f9eed7b6cfd5a02c83eba1fbaae
MichaelShie <107825316+MichaelShie@users.noreply.github.com>
2022-06-20 12:00:21 +0800

    Add files via upload
    Add Python Sample

---
+++ Python3/auth_TDX.py
@@ -0,0 +1,53 @@
+import requests
+from pprint import pprint
+import json
+
+app_id = 'XXXXX-XXXXXXXX-XXXX-XXXX'
+app_key = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+
+auth_url="https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token"
+url = "https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveTrainDelay?$top=30&$format=JSON"
+
+class Auth():
+
+    def __init__(self, app_id, app_key):
+        self.app_id = app_id
+        self.app_key = app_key
+
+    def get_auth_header(self):
+        content_type = 'application/x-www-form-urlencoded'
+        grant_type = 'client_credentials'
+
+        return{
+            'content-type' : content_type,
+            'grant_type' : grant_type,
+            'client_id' : self.app_id,
+            'client_secret' : self.app_key
+        }
+
+class data():
+
+    def __init__(self, app_id, app_key, auth_response):
+        self.app_id = app_id
+        self.app_key = app_key
+        self.auth_response = auth_response
+
+    def get_data_header(self):
+        auth_JSON = json.loads(self.auth_response.text)
+        access_token = auth_JSON.get('access_token')
+
+        return{
+            'authorization': 'Bearer '+access_token
+        }
+
+if __name__ == '__main__':
+    a = Auth(app_id, app_key)
+    auth_response = requests.post(auth_url, a.get_auth_header())
+    d = data(app_id, app_key, auth_response)
+    data_response = requests.get(url, headers=d.get_data_header())
+    print(auth_response)
+    pprint(auth_response.text)
+    print(data_response)
+    pprint(data_response.text)
+
+
