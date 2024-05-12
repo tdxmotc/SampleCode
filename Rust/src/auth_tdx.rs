8989d913d561d6c2feac69a09568ca016fd04431
chelsea <chelseawen03@gmail.com>
2024-05-11 20:56:02 -0700

    cleaned version without HashMap for example

--- Rust/src/auth_tdx.rs
+++ Rust/src/auth_tdx.rs
@@ -3,24 +3,19 @@ use reqwest::header::AUTHORIZATION;
 use reqwest::{header::CONTENT_TYPE, *};
 use serde_json::*;
 use std::error::Error;
-use std::{collections::HashMap, fs::File, path::Path};
 
 static AUTH_URL: &str = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
 static TEST_URL: &str = "https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveBoard/Station/1000?$filter=Direction eq 1&$format=JSON";
 
+static CLIENT_ID: &str = "XXXXX-XXXXXXXX-XXXX-XXXX";
+static CLIENT_SECRET: &str = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    //可以用處純在其他目錄裡的JSON檔案保護使用者的Access Token
-    let file_path = Path::new("C:\\Users\\bookw\\Downloads\\tdx-secret.json");
-    //let file_path = Path::new("C:\\Users\\..\\sample-secret.json");
-    let file = File::open(file_path).expect("file not found");
-    let secret: HashMap<String, String> =
-        serde_json::from_reader(file).expect("error while reading");
-
     let auth_header = json!({
         "grant_type": "client_credentials",
-        "client_id": secret.get("client_id").unwrap(),
-        "client_secret": secret.get("client_secret").unwrap()
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
     });
 
     let client = Client::new();
