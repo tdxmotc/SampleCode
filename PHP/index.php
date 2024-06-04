9672b253081e5c276deb2ac2499cd614d7da6672
tdxmotc <44422898+tdxmotc@users.noreply.github.com>
2024-06-04 18:29:16 +0800

    啟用gzip

--- PHP/index.php
+++ PHP/index.php
@@ -8,7 +8,6 @@ curl_setopt($ch, CURLOPT_URL, 'https://tdx.transportdata.tw/auth/realms/TDXConne
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=client_credentials&client_id='.$client_id.'&client_secret='.$client_secret);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_ENCODING , 'gzip');//啟用gzip
 $result = curl_exec($ch);
 curl_close($ch);
 
@@ -16,10 +15,11 @@ $access_token = json_decode($result,1)['access_token'];
 
 // 測試：取得新北市公車到站資料
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/NewTaipei?$top=30&$format=JSON');
+curl_setopt($ch, CURLOPT_URL, 'https://tdx.transportdata.tw/api/basic/v2/Bus/EstimatedTimeOfArrival/City/NewTaipei?$top=10&$format=JSON');
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('authorization: Bearer '.$access_token));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_ENCODING , 'gzip');//啟用gzip
 $busEstimatedTime = curl_exec($ch);
 curl_close($ch);
 print_r($busEstimatedTime);
